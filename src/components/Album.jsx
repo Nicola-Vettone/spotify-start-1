841f05b0b5fb847cee5524a05772c5a752b847d8
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-21 16:51:41 +0100

    fetch action

---
+++ src/components/Album.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Container, Row, Spinner, Card, Col } from "react-bootstrap";
+import { getMusic } from "../redux/action";
+
+const Album = ({ artist, genre }) => {
+  const dispatch = useDispatch();
+  const songs = useSelector((state) => state.music.content);
+
+  useEffect(() => {
+    dispatch(getMusic(artist));
+  }, [dispatch, artist]);
+
+  return (
+    <Container className="my-4">
+      <h3>{genre} Music</h3>
+      <Row>
+        {songs.length === 0 && <Spinner animation="border" />}
+        {songs.map((song) => (
+          <Col key={song.id} md={3}>
+            <Card>
+              <Card.Img variant="top" src={song.album.cover_xl} />
+              <Card.Body>
+                <Card.Title>{song.title}</Card.Title>
+                <Card.Text>{song.artist.name}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Album;
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,3 +1,5 @@
+import Album from "./Album";
+
 function MainSection() {
   return (
     <>
@@ -11,39 +13,8 @@ function MainSection() {
             <a href="#">DISCOVER</a>
           </div>
         </div>
-        <div className="row">
-          <div className="col-10">
-            <div id="rock">
-              <h2>Rock Classics</h2>
-              <div
-                className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-                id="rockSection"
-              ></div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-10">
-            <div id="pop">
-              <h2>Pop Culture</h2>
-              <div
-                className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-                id="popSection"
-              ></div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-10">
-            <div id="hiphop">
-              <h2>#HipHop</h2>
-              <div
-                className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-                id="hipHopSection"
-              ></div>
-            </div>
-          </div>
-        </div>
+
+        <Album artist="tedua" genre="Rock Classics" />
       </main>
     </>
   );
