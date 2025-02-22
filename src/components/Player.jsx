879b2e9552462ef33db962a870ae3a5193af4630
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-22 12:42:02 +0100

    a

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,15 +7,15 @@ function Player() {
     <div className="container-fluid fixed-bottom bg-container pt-1">
       <div className="row h-100">
         <div className="col-lg-10 offset-lg-2">
-          <div className="row h-100 flex-column justify-content-center align-items-center">
+          {player && (
+            <div className="d-flex ">
+              <img src={player.image} alt="" width={60} />
+              <p className="text-white">{player.title}</p>
+            </div>
+          )}
+          <div className="row h-100  justify-content-center align-items-center">
             <div className="col-6 col-md-4 playerControls">
               <div className="d-flex">
-                {player && (
-                  <div className="d-flex">
-                    <img src={player.image} alt="" width={60} />
-                    <p className="text-white">{player.title}</p>
-                  </div>
-                )}
                 <a href="#">
                   <img src="assets/playerbuttons/shuffle.png" alt="shuffle" />
                 </a>
